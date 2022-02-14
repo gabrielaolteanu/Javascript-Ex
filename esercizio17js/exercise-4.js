27439c5496d997aaaa5cb8b12d5b9cbddbf77b1b
Gabriela Olteanu <gabrielaolteanu15@gmail.com>
2022-02-14 16:34:54 +0100

    Esercizio 17

--- esercizio17js/exercise-4.js
+++ esercizio17js/exercise-4.js
@@ -1,6 +1,34 @@
 function calculate() {
-  // ...
+  let result = 0
+  return {
+   add (num) {
+      result+= num
+      return this
+    },
+
+    sub (num) {
+      result -= num 
+      return this
+    },
+
+    multiply(num) {
+      result *= num
+      return this
+    },
+
+    divide(num) {
+      result /= num
+    return this
+    },
+    printResult()
+    {
+      console.log(result)
+      return this
+    }
+  }
+
 }
+
 
 const calculator = calculate();
 calculator.add(2).add(4).multiply(3).sub(1).sub(3).divide(2).printResult(); // Il risultato sarà: 7
