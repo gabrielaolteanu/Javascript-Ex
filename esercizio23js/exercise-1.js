489d8abc8d0ed93a97903a2168e4914349a94a59
Gabriela Olteanu <gabrielaolteanu15@gmail.com>
2022-02-14 17:20:55 +0100

    Esercizio 23

--- esercizio23js/exercise-1.js
+++ esercizio23js/exercise-1.js
@@ -3,5 +3,6 @@ const person = {
   lastName: 'Rossi',
   age: 25
 }
+Object.entries(person).forEach(([chiave, valore])=> console.log( `${chiave}: ${valore} `)) 
 
 // Print values of person using Object.keys
