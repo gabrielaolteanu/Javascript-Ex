88786da16c2a0bbe263c30e24c26fca9eb3b34ea
Gabriela Olteanu <gabrielaolteanu15@gmail.com>
2022-02-15 10:51:53 +0100

    Aggiunta EX25

--- esercizio25js/exercise-1.js
+++ esercizio25js/exercise-1.js
@@ -1,6 +1,31 @@
 const person = {
-  // ...
+ get firstName() {
+   return this.name
+ },
+ set firstName(firstName){
+   this.name= firstName
+ },
+
+ get lastName() {
+  return this.surname
+},
+set lastName(lastName){
+  this.surname= lastName
+},
+
+fullName: function(){
+  return this.name +  " "  + this.surname
 }
+
+}
+
+const john = Object.create(person) 
+john.firstName = `John`;
+john.lastName = "Doe";
+
+const simon = Object.create(person)
+simon.firstName = "Simon";
+simon.lastName = "Collins";
 
 console.log(john.fullName()); // John Doe
 console.log(simon.fullName()); // Simon Collins
