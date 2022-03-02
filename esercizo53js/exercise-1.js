e9ff79939e24f7678b5988815ada1f4259a3014d
Gabriela Olteanu <gabrielaolteanu15@gmail.com>
2022-03-02 15:18:02 +0100

    Esercizi

--- esercizo53js/exercise-1.js
+++ esercizo53js/exercise-1.js
@@ -1,5 +1,6 @@
-function sum(num1, num2, num3, num4, num5) {
-    return num1 + num2 + num3 + num4 + num5;
+function sum(...num) {
+return num.reduce((somma, current) => {
+return  somma + current})
 }
 
 console.log(sum(1, 2, 3, 4, 5));
